48fbbca0586b880d4dfb686dd141d56aababd2b9
EvanilsonP <pevanilson99@gmail.com>
2023-08-03 10:52:50 -0300

    Wrap-up

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
-const tbody = document.querySelector('tbody');
-const descItem = document.querySelector('#desc');
-const amount = document.querySelector('#amount');
-const type = document.querySelector('#type');
-const btnNew = document.querySelector('#btnNew');
+const tbody = document.querySelector("tbody");
+const descItem = document.querySelector("#desc");
+const amount = document.querySelector("#amount");
+const type = document.querySelector("#type");
+const btnNew = document.querySelector("#btnNew");
 
-const incomes = document.querySelector('.incomes');
-const expenses = document.querySelector('.expenses');
-const total = document.querySelector('.total');
+const incomes = document.querySelector(".incomes");
+const expenses = document.querySelector(".expenses");
+const total = document.querySelector(".total");
 
 let items;
 
-function deleteItem(index) {
-    items.splice(index, 1);
-    setItemsDB();
-    loadItems();
+btnNew.onclick = () => {
+  if (descItem.value === "" || amount.value === "" || type.value === "") {
+    return alert("Preencha todos os campos!");
+  }
+// Se tiver todas as informações
+  items.push({
+    desc: descItem.value,
+    amount: Math.abs(amount.value).toFixed(2),
+    type: type.value,
+  });
+
+  setItensBD();
+
+  loadItens();
+
+  descItem.value = "";
+  amount.value = "";
 };
 
-function insertItem(item, index) {
-    let tr = document.createElement('tr');
+function deleteItem(index) {
+  items.splice(index, 1);
+  setItensBD();
+  loadItens();
+}
 
-    tr.innerHTML = `
+function insertItem(item, index) {
+  let tr = document.createElement("tr");
+
+  tr.innerHTML = `
     <td>${item.desc}</td>
     <td>R$ ${item.amount}</td>
     <td class="columnType">${
       item.type === "Entrada"
-        ? '<i class="bx bxs-chevron-up-circle"></i>'   // Icon seta p cima
-        : '<i class="bx bxs-chevron-down-circle"></i>' // Icon seta p baixo
+      ? '<i class="bx bxs-chevron-up-circle"></i>'   // Icon seta p cima
+      : '<i class="bx bxs-chevron-down-circle"></i>' // Icon seta p baixo
     }</td>
     <td class="columnAction">
       <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
@@ -33,18 +52,47 @@ function insertItem(item, index) {
   `;
 
   tbody.appendChild(tr);
-};
+}
 
-function loadItems() {
-    items = getItemsDB();
-    // Limpar tbody para não duplicar itens na tela
-    tbody.innerHTML = '';
-    items.forEach((item, index) => {
-        insertItem(item, index);
-    })
-};
+function loadItens() {
+  items = getItensBD();
+  // Limpar tbody para não duplicar itens na tela
+  tbody.innerHTML = "";
+  items.forEach((item, index) => {
+    insertItem(item, index);
+  });
 
+  getTotals();
+}
+
+function getTotals() {
+    // Pega o array de itens e filtra os itens do tipo entrada e depois um map que pega as informações de amount/valor
+  const amountIncomes = items
+    .filter((item) => item.type === "Entrada")
+    .map((transaction) => Number(transaction.amount));
+
+  const amountExpenses = items
+    .filter((item) => item.type === "Saída")
+    .map((transaction) => Number(transaction.amount));
+// Soma as entrada e reduz a uma única variável 
+  const totalIncomes = amountIncomes
+    .reduce((acc, cur) => acc + cur, 0)
+    .toFixed(2); // Cria duas casas decimais
+// Acumulando o valor para uma única variável
+  const totalExpenses = Math.abs(
+    amountExpenses.reduce((acc, cur) => acc + cur, 0)
+  ).toFixed(2);
+
+  const totalItems = (totalIncomes - totalExpenses).toFixed(2);
+
+  incomes.innerHTML = totalIncomes;
+  expenses.innerHTML = totalExpenses;
+  total.innerHTML = totalItems;
+}
 // Pega os itens no banco - Se não, será um array vazio
-const getItemsDB = () => JSON.parse(localStorage.getItem('db_items')) ?? [];
+const getItensBD = () => JSON.parse(localStorage.getItem("db_items")) ?? [];
 // Inserindo no banco as informações da variável items
-const setItemsDB = () => localStorage.setItem('db_items', JSON.stringify(items));
+const setItensBD = () =>
+  localStorage.setItem("db_items", JSON.stringify(items));
+
+loadItens();
