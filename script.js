88f3ba3a1ac1acac9d1249cd59c674a78e66dcc3
EvanilsonP <pevanilson99@gmail.com>
2023-08-03 10:18:26 -0300

    Criando funções localStorage

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ const btnNew = document.querySelector('#btnNew');
 const incomes = document.querySelector('.incomes');
 const expenses = document.querySelector('.expenses');
 const total = document.querySelector('.total');
+
+let items;
+// Pega os itens no banco - Se não, será um array vazio
+const getItemDB = () => JSON.parse(localStorage.getItem('db_items')) ?? [];
+// Inserindo no banco as informações da variável items
+const setItemDB = () => localStorage.setItem('db_items', JSON.stringify(items));
